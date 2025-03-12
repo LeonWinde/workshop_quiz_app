04537031b1498e3800d0f9af3da93a648bfa716d
LeonWinde <leon.winde@dci-student.org>
2025-03-12 11:27:10 +0100

    making questions available

--- index.ts
+++ index.ts
@@ -1,42 +1,54 @@
-const prompt = require("prompt-sync")();
+//@ts-ignore
+import prompt from "picoprompt";
+
+
 
 type Question = {
   questionText: string;
   answerOptions: string[];
   correctIndex: number;
 };
+const getQuestions = async () =>{
+  try {
+    const res = await fetch("/home/dci-student/Schreibtisch/workshop/workshop_quiz_app/questions.json")
+    const json = await res.json();
+    console.log('res',json);
+  } catch (error) {
+    console.log('error',error);
+  }
+}
+getQuestions();
+// const testQuestion: Question = {
+//   questionText: "what is my name",
+//   answerOptions: ["1. Leon", "2. Anton", "3. Tom", "4. John"],
+//   correctIndex: 0,
+// };
 
-const testQuestion: Question = {
-  questionText: "what is my name",
-  answerOptions: ["1. Leon", "2. Anton", "3. Tom", "4. John"],
-  correctIndex: 0,
-};
-
-const testQuestion2: Question = {
-  questionText: "where do i live",
-  answerOptions: ["1. Berlin", "2. Hannover", "3. Hamburg", "4. Koeln"],
-  correctIndex: 2,
-};
+// const testQuestion2: Question = {
+//   questionText: "where do humans live",
+//   "answerOptions": ["1. Earth", "2. Mars", "3. Jupiter", "4. Venus"],
+//   "correctIndex": "2",
+// };
 const questionList: Question[] = [];
 
-questionList.push(testQuestion);
-questionList.push(testQuestion2);
+// questionList.push(testQuestion);
+// questionList.push(testQuestion2);
 
-let score: number = 0;
+// let score: number = 0;
 
-while (questionList.length !== 0) {
-  const { questionText, answerOptions, correctIndex } = questionList[0];
-  console.log(`QUESTION : \n${questionText}\n\n${answerOptions.join("\n")}`);
-  const userAnswer = Number(prompt("What is the right answer? "));
-  console.log(userAnswer);
-  if (userAnswer - 1 == correctIndex) {
-    score++;
-    console.log("Congratulations that was the right answer!");
-  } else {
-    console.log(
-      `That was the wrong answer.\n${answerOptions[correctIndex]} was the right answer`
-    );
-  }
-  questionList.shift();
-}
-console.log(score);
+// while (questionList.length !== 0) {
+//   const { questionText, answerOptions, correctIndex } = questionList[0];
+//   console.log(`QUESTION : \n\n${questionText}\n\n${answerOptions.join("\n")}`);
+//   const userAnswer:number = Number(prompt("What is the right answer? "));
+//   if (userAnswer - 1 === correctIndex) {
+//     score++;
+//     console.log("Congratulations that was the right answer!");
+//   } else {
+//     console.log(
+//       `That was the wrong answer.\n${answerOptions[correctIndex]} was the right answer\n`
+//     );
+
+//   }
+//   questionList.shift();
+// }
+// console.log(score);
